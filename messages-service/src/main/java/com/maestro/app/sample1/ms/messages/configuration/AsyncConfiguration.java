a1c28d6bfc41302d37aad984eccce06b9349dfb0
dev-otitarenko <76594207+dev-otitarenko@users.noreply.github.com>
2021-01-10 20:02:43 +0100

    Fixed (#8)

---
+++ messages-service/src/main/java/com/maestro/app/sample1/ms/messages/configuration/AsyncConfiguration.java
@@ -0,0 +1,41 @@
+package com.maestro.app.sample1.ms.messages.configuration;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration implements AsyncConfigurer, ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
+    @Bean(name = "threadPoolMessagesExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        CustomASYNCErrorHandler handler = new CustomASYNCErrorHandler();
+        handler.setApplicationContext(applicationContext);
+        return handler;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
--- messages-service/src/main/java/com/maestro/app/sample1/ms/messages/listeners/BroadcastMessagesProcessor.java
+++ messages-service/src/main/java/com/maestro/app/sample1/ms/messages/listeners/BroadcastMessagesProcessor.java
@@ -1,5 +1,6 @@
 package com.maestro.app.sample1.ms.messages.listeners;
 
+import com.maestro.app.sample1.ms.messages.services.UserMessagesService;
 import com.maestro.app.sample1.ms.messages.utils.Constants;
 import com.maestro.app.utils.queue.QueueBroadcastMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -11,11 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 @RabbitListener(queues = Constants.QUEUE_BROADCASTMESSAGE_NAME)
 public class BroadcastMessagesProcessor {
+    private final UserMessagesService messageService;
+
+    public BroadcastMessagesProcessor(UserMessagesService messageService) {
+        this.messageService = messageService;
+    }
+
     @RabbitHandler
     public void processEvent(final QueueBroadcastMessage event) {
         log.debug(" -> [UserMessageProcessor]  message [{}] received", event);
-        //
-        // This broadcast messsage must be sent to all users
-        //
+        messageService.saveMessage(event);
     }
 }
