b8787e4228ceb59b67fe392544ec478d60bc693b
oleksii titarenko <atitarenko.eng@gmail.com>
2021-01-17 17:02:13 +0100

    events-service: small changes

--- events-service/src/main/java/com/maestro/app/sample/ms/events/configuration/ResourceServerConfiguration.java
+++ events-service/src/main/java/com/maestro/app/sample/ms/events/configuration/ResourceServerConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -19,6 +20,7 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 @Configuration
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Profile("!test")
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
--- events-service/src/main/java/com/maestro/app/sample/ms/events/listeners/LogConnectsProcessor.java
+++ events-service/src/main/java/com/maestro/app/sample/ms/events/listeners/LogConnectsProcessor.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@RabbitListener(queues = Constants.QUEUE_LOGSCONNECTS_NAME)
+@RabbitListener(id = "events-listener-connects", queues = Constants.QUEUE_LOGSCONNECTS_NAME)
 public class LogConnectsProcessor {
     private final LogConnectEventsService evtService;
 
